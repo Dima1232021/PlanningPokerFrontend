294ea94140731ce6ba8cdf4c65bc8778b945007a
Dima1232021 <moarobota584@gmail.com>
2021-11-20 22:31:41 +0200

    getting users

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../../config";
+import { ActionCable } from "react-actioncable-provider";
 import "./main.scss";
 
 export default function MainPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/new_game/users`, {}).then((value) =>
+      value.json().then((data) => {
+        // setUsers(data);
+        console.log(data);
+      })
+    );
+  }, []);
+  console.log(users);
+
   return (
     <div className="main">
+      {
+        <ActionCable
+          channel={{ channel: "ShowUsersChannel" }}
+          onReceived={(value) => {
+            console.log("value", value);
+          }}
+        />
+      }
       <h1>Main page</h1>
+      <h2>користувачі</h2>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./reducers/index";
+import { ActionCableProvider } from "react-actioncable-provider";
+import { API_WS_URL } from "./config";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ActionCableProvider url={API_WS_URL}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ActionCableProvider>,
   document.getElementById("root")
 );
