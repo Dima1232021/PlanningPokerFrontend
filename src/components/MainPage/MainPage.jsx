89b82e1feca3537eff14b316a73e996f4b8a392d
Dima1232021 <moarobota584@gmail.com>
2021-11-22 00:07:15 +0200

    made 2 functions, add and remove user

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -4,30 +4,66 @@ import { ActionCable } from "react-actioncable-provider";
 import "./main.scss";
 
 export default function MainPage() {
+  const [numberOfUsers, setNumberOfUsers] = useState(null);
   const [users, setUsers] = useState([]);
+  const [addPlayers, setAddPlayers] = useState([]);
 
   useEffect(() => {
-    fetch(`${API_URL}/new_game/users`, {}).then((value) =>
+    fetch(`${API_URL}/users/show`, {
+      credentials: "include",
+    }).then((value) =>
       value.json().then((data) => {
-        // setUsers(data);
+        setUsers(data);
         console.log(data);
       })
     );
-  }, []);
-  console.log(users);
+  }, [numberOfUsers]);
+  console.log(addPlayers);
 
+  function addUser(user) {
+    let findUser = addPlayers.find((value) => value === user);
+    !findUser && setAddPlayers([...addPlayers, user]);
+  }
+  function deleteUser(user) {
+    let arr = addPlayers.filter((value) => value !== user);
+    console.log(arr);
+    setAddPlayers(arr);
+  }
   return (
     <div className="main">
       {
         <ActionCable
           channel={{ channel: "ShowUsersChannel" }}
           onReceived={(value) => {
-            console.log("value", value);
+            setNumberOfUsers(value);
           }}
         />
       }
       <h1>Main page</h1>
       <h2>користувачі</h2>
+      <div className="main__show-user">
+        <ul>
+          {users.map((user) => {
+            return (
+              <li key={user.id} onClick={() => addUser(user)}>
+                {user.username}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <h2>користувачі які ви плануєте пригласити до гри</h2>
+      <div className="main__show-user">
+        <ul>
+          {addPlayers.map((user) => {
+            return (
+              <li key={user.id} onClick={() => deleteUser(user)}>
+                {user.username}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
