c9d3f3c0b19a923a036690186d6c82f770f696f1
Dima1232021 <moarobota584@gmail.com>
2021-12-03 12:09:39 +0200

    Створив нову сторінку Гра добавив стилі та Вивід назви гри та ім'я ведучого

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -8,14 +8,31 @@ export default function GamePage() {
   const dispatch = useDispatch();
   const game = useSelector((state) => state.games.gameYouHaveJoined);
 
+  console.log(game);
+
   function leave() {
     dispatch(leaveTheGame(game));
   }
 
   return (
     <div className="game">
-      <h1>Game page</h1>
-      <button onClick={leave}>Вийти з гри</button>
+      <div className="game__column">
+        <div className="game__header">
+          <h2 className="game__title">{game.name_game}</h2>
+          <p>
+            <span>Ведучий гри: {game.driving.user_name}</span>
+          </p>
+        </div>
+        <div className="game__form form"></div>
+      </div>
+      <div className="game__column">
+        <div className="game__menu menu">
+          <div className="menu__row">
+            <h2 className="menu__title">Menu</h2>
+            <button onClick={leave}>Вийти</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
