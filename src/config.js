0c853288877c839905a11dd1c761e3c45cc38890
Dima1232021 <moarobota584@gmail.com>
2021-12-15 19:54:18 +0200

    змінив в config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = `http://localhost:3000`;
-export const API_WS_URL = "ws://localhost:3000/cable";
+export const API_URL = `https://planning-poker-backend-atom.herokuapp.com`;
+export const API_WS_URL = "https://planning-poker-backend-atom.herokuapp.com/cable";
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
