3f7ddb2872eaf5af79789bbac30e0bd0c65de844
Dima1232021 <moarobota584@gmail.com>
2021-12-17 15:10:40 +0200

    створив новий компонент Timer.jsx

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // "start": "PORT=3001 react-scripts start",
-// export const API_URL = "https://devserver.test";
-// export const API_WS_URL = "wss://devserver.test/cable";
-export const API_URL = "https://planning-poker-backend-atom.herokuapp.com";
-export const API_WS_URL =
-  "wss://planning-poker-backend-atom.herokuapp.com/cable";
+export const API_URL = "https://devserver.test";
+export const API_WS_URL = "wss://devserver.test/cable";
+// export const API_URL = "https://planning-poker-backend-atom.herokuapp.com";
+// export const API_WS_URL =
+//   "wss://planning-poker-backend-atom.herokuapp.com/cable";
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
