8e2bd7132bc883a3bc2273b65350fc4f937db8c2
Dima1232021 <moarobota584@gmail.com>
2022-01-03 13:57:21 +0200

    Додав папку pages

--- src/components/GamePage/menu/MenuPlayers/MenuPlayers.jsx
+++ src/components/GamePage/menu/MenuPlayers/MenuPlayers.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import UsersBlock from "../../../usersBlock/UsersBlock";
 import MenuBlock from "../../../menuBlock/MenuBlock";
 import { declineInvitation } from "../../../../actions/Game";
+import Modal from "../../../Modal/Modal";
 
 import "./menuPlayers.scss";
 
@@ -13,37 +14,54 @@ export default function MenuPlayers() {
   const gameId = useSelector((state) => state.games.gameId);
   const userid = useSelector((state) => state.user.userid);
   const [infoPlayers, setInfoPlayers] = useState("In the game");
+  const [active, setActive] = useState(false);
 
   function expelPlayer(user) {
     declineInvitation(gameId, user.id);
   }
 
+  function addPlayer() {
+    setActive(true);
+    console.log("addplayer");
+  }
+
   return (
-    <div className="menu-players">
-      <MenuBlock
-        btn1="In the game"
-        btn2="Invited players"
-        value={infoPlayers}
-        setValue={setInfoPlayers}
-      />
-      {infoPlayers == "In the game" ? (
-        <UsersBlock value={onlineUsers} keyValue={"id"} name={"username"} />
-      ) : (
-        <div className="usersBlock">
-          <ul className="usersBlock__list">
-            {invitedUsers.map((val) => {
-              return (
-                <li key={val.id} className="usersBlock__link">
-                  <span>{val.username}</span>
-                  {val.id != userid && game.driving.user_id == userid && (
-                    <button onClick={() => expelPlayer(val)}>Expel</button>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <div className="menu-players">
+        <MenuBlock
+          btn1="In the game"
+          btn2="Invited players"
+          value={infoPlayers}
+          setValue={setInfoPlayers}
+        />
+        {infoPlayers == "In the game" ? (
+          <UsersBlock value={onlineUsers} keyValue={"id"} name={"username"} />
+        ) : (
+          <>
+            <div className="usersBlock">
+              <ul className="usersBlock__list">
+                {invitedUsers.map((val) => {
+                  return (
+                    <li key={val.id} className="usersBlock__link">
+                      <span>{val.username}</span>
+                      {val.id != userid && game.driving.user_id == userid && (
+                        <button onClick={() => expelPlayer(val)}>Expel</button>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="menu-players__row">
+              <button onClick={addPlayer}>Add Player</button>
+            </div>
+          </>
+        )}
+      </div>
+
+      <Modal active={active} setActive={setActive}>
+        <h1>asdfasd sadfa sdfasd f ds</h1>
+      </Modal>
+    </>
   );
 }
