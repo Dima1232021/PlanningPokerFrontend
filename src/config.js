41034b240d1d80bd566b3cc5796de65671b74df1
Dima1232021 <moarobota584@gmail.com>
2022-01-24 12:44:19 +0200

    підхотовив для  деплоя на хероку

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 // "start": "PORT=3001 react-scripts start",
-export const API_URL = "https://devserver.test";
-export const API_WS_URL = "wss://devserver.test/cable";
-// export const API_URL = "https://planning-poker-backend-atom.herokuapp.com";
-// export const API_WS_URL =
-//   "wss://planning-poker-backend-atom.herokuapp.com/cable";
+// export const API_URL = "https://devserver.test";
+// export const API_WS_URL = "wss://devserver.test/cable";
+export const API_URL = "https://planning-poker-backend-atom.herokuapp.com";
+export const API_WS_URL = "wss://planning-poker-backend-atom.herokuapp.com/cable";
 
 export const bodyFetch = (url, data = false) => {
   if (data) {
